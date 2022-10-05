2464f5e64b96cc48697bc57be8980ed9133db9d6
unknown <gly6@cornell.edu>
2022-10-04 20:36:15 -0400

    update changes to the iris

--- eyes.py
+++ eyes.py
@@ -77,7 +77,7 @@ if JOYSTICK_X_IN >= 0 or JOYSTICK_Y_IN >= 0 or PUPIL_IN >= 0:
 
 # Load SVG file, extract paths & convert to point lists --------------------
 
-dom               = parse("graphics/eye2.svg")
+dom               = parse("graphics/eye2_change.svg")
 vb                = get_view_box(dom)
 pupilMinPts       = get_points(dom, "pupilMin"      , 32, True , True )
 pupilMaxPts       = get_points(dom, "pupilMax"      , 32, True , True )
